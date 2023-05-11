2932613293d8fd40d1b01eb96d6661825f11ef04
Beomjun Kim <121419945+kxxbeomjun@users.noreply.github.com>
2023-05-11 22:00:37 +0900

    Add files via upload

---
+++ programmers/Level 1/핸드폰 번호 가리기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    
+    int size = phone_number.size();
+    
+    for(int i=0; i<size-4; i++){
+        answer += '*';
+    }
+    
+    for(int i=size-4; i < size; i++){
+        answer += phone_number[i];
+    }
+    
+    return answer;
+}
