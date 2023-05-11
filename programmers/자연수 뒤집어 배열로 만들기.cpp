03094c8387c8afc5fb7bc5b7587746f302b6d57f
Beomjun Kim <121419945+kxxbeomjun@users.noreply.github.com>
2023-05-11 17:17:00 +0900

    Update 자연수 뒤집어 배열로 만들기.cpp

--- programmers/자연수 뒤집어 배열로 만들기.cpp
+++ programmers/자연수 뒤집어 배열로 만들기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> solution(long long n) {
     vector<int> answer;
     
-    while true{
+    while (true){
         answer.push_back(n%10); #1의 자리수 가지고 오기
         n = n/10; #1의 자리수 버리기
         if (n == 0){
